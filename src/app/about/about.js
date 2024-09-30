49364490e68a196cc919f58078af052aa9a29c6b
Pedro Alves <pedro.alves6491@gmail.com>
2024-09-30 18:07:42 -0300

    Prototipo

---
+++ src/app/about/about.js
@@ -0,0 +1,3 @@
+export default function About(){
+    return <h1>Teste</h1>
+}
---
+++ src/app/components/Navbar/page.js
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+import logoimg from '../../public/logo.png'
+import styles from './Navbar.module.css'
+import "../../src/app/globals.css";
+
+export default function Navbar(){
+    return(
+        <nav className={styles.nav}>
+            <Image src={logoimg} className={styles.logoimg}></Image>
+            <div className={styles.div}>
+                <select>
+                    <option>Produtos</option>
+                    <option>Option</option>
+                    <option>Option</option>
+                    <option>Option</option>
+                </select>
+                <Link href="/About">Sobre nós</Link>
+                <Link href="#">Projetos</Link>
+                <Link href="#">Contate nos</Link>
+            </div>
+        </nav>
+    )
+}
